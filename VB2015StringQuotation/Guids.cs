c47a856510b16f8c685a4e5369c5d1039bcdb6ff
gekka <git-gekka@hotmail.com>
2016-09-05 01:17:17 +0900

    Ver 1.00

---
+++ VB2015StringQuotation/Guids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gekka.VisualStudio.Extention
+{
+    static class GuidList
+    {
+        public const string guidVSPackage1PkgString = "dc04701e-ea51-479f-b085-1cdf2f089534";
+        public const string guidVSPackage1CmdSetString = "45bd9c15-b0af-4e36-941d-20358f985533";
+
+        public static readonly Guid guidVSPackage1CmdSet = new Guid(guidVSPackage1CmdSetString);
+    };
+}
---
+++ VB2015StringQuotation/VB2015StringQuotationPackage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+
+namespace Gekka.VisualStudio.Extention
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidVSPackage1PkgString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
+    public sealed class VB2015StringQuotationPackage : Package
+    {
+        private EnvDTE80.DTE2 dte;
+        private EnvDTE80.Events2 event2;
+        private EnvDTE80.TextDocumentKeyPressEvents keyPressEvents;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            this.dte = (EnvDTE80.DTE2)GetService(typeof(EnvDTE.DTE));
+            this.event2 = (EnvDTE80.Events2)this.dte.Events;
+
+            this.keyPressEvents = this.event2.TextDocumentKeyPressEvents;
+            this.keyPressEvents.AfterKeyPress += TextDocumentKeyPressEvents_AfterKeyPress;
+        }
+
+        void TextDocumentKeyPressEvents_AfterKeyPress(string Keypress, EnvDTE.TextSelection Selection, bool InStatementCompletion)
+        {
+            try
+            {
+
+                if (Keypress == "\r" && Selection.Parent.Language == "Basic")
+                {
+                    Selection.LineUp();
+                    try
+                    {
+                        Selection.StartOfLine(EnvDTE.vsStartOfLineOptions.vsStartOfLineOptionsFirstText);
+                        Selection.EndOfLine(true);
+
+                        string text = Selection.Text;
+                        if (text.Length > 0)
+                        {
+                            int q = 0;
+                            foreach (char c in text)
+                            {
+                                if (c == '\"')
+                                {
+                                    q++;
+                                }
+                                else if (c == '\'')
+                                {
+                                    if ((q & 1) == 0)
+                                    {
+                                        return;
+                                    }
+
+                                }
+                            }
+
+                            if ((q & 1) == 1)
+                            {
+                                Selection.MoveToPoint(Selection.BottomPoint);
+                                Selection.Insert("\"");
+                            }
+                        }
+
+                    }
+                    finally
+                    {
+                        Selection.LineDown();
+                        Selection.StartOfLine(EnvDTE.vsStartOfLineOptions.vsStartOfLineOptionsFirstText);
+
+                        //dte.ExecuteCommand("Edit.DocumentFormat");
+                    }
+
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
